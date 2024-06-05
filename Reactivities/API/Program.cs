6617630f16a9149bba93ecb36ea9e096ef2d677b
Hrithik <hrithikmistry2000@gmail.com>
2024-06-05 12:03:13 +0530

    created client side

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -9,11 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(opt=>{
+opt.AddPolicy("CorsPolicy",policy => {
+    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
