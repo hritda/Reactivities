dcf6e2c9c3ff78d6549a0f16c0a8ab2ccfa26ff9
Hrithik <hrithikmistry2000@gmail.com>
2024-06-08 18:14:51 +0530

    made some changes

--- Reactivities/API/Program.cs
+++ Reactivities/API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Application.Activities;
 using Application.core;
 using Microsoft.EntityFrameworkCore;
@@ -8,26 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
